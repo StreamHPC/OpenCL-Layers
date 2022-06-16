6e9d681d8b296a7d085bcab98ead3df26a5de83c
Robin Voetter <robin@streamhpc.com>
2022-06-16 16:50:37 +0200

    fix shadowed variable

--- param-verification/param_verification.cpp
+++ param-verification/param_verification.cpp
@@ -34,16 +34,16 @@ namespace layer {
     const auto parser =
       ocl_layer_utils::settings_parser("param_verification", settings_from_file);
 
-    auto settings = layer_settings{};
+    auto result = layer_settings{};
     const auto debug_log_values =
       std::map<std::string, DebugLogType>{{"stdout", DebugLogType::StdOut},
                                           {"stderr", DebugLogType::StdErr},
                                           {"file", DebugLogType::File}};
-    parser.get_enumeration("log_sink", debug_log_values, settings.log_type);
-    parser.get_filename("log_filename", settings.log_filename);
-    parser.get_bool("transparent", settings.transparent);
+    parser.get_enumeration("log_sink", debug_log_values, result.log_type);
+    parser.get_filename("log_filename", result.log_filename);
+    parser.get_bool("transparent", result.transparent);
 
-    return settings;
+    return result;
   }
 }
 
